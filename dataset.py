88fec398e04ddb3c54aa0436d5544dec99ab3995
OliverPan <397828546@qq.com>
2018-09-15 18:35:15 +0800

    0915modification

--- dataset.py
+++ dataset.py
@@ -9,3 +9,29 @@ class Data(object):
         else:
             self.label = label
 
+
+class Dataset(object):
+    def __init__(self, dataset):
+        feature_lis = []
+        label_lis = []
+        num=0
+        for data in dataset:
+            feature_lis.append(data.feature)
+            label_lis.append(data.label)
+            num += 1
+        self.features = np.array(feature_lis, dtype=np.int32)
+        self.labels = np.array(label_lis, dtype=np.int32)
+        self.num = num
+        self.index = 0
+
+    def next_batch(self, batch_size):
+        if self.index + batch_size < self.num:
+            return_set = self.features[self.index:self.index+batch_size], self.labels[self.index:self.index+batch_size]
+        else:
+            return_set = \
+                (np.append(self.features[self.index:], self.features[:(self.index + batch_size) % self.num], axis=0),
+                 np.append(self.labels[self.index:], self.labels[:(self.index + batch_size) % self.num], axis=0))
+        self.index = (self.index + batch_size) % self.num
+        return return_set
+
+
--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from dataset import Data
+from dataset import Data, Dataset
 import random
 
 
@@ -34,11 +34,11 @@ def data_import(data_file):
 
 
 # 数据集相关常数
-INPUT_NODE = 9
-OUTPUT_NODE = 3
+INPUT_NODE = 32
+OUTPUT_NODE = 2
 
 # 神经网络的参数
-LAYER1_NODE = 5
+LAYER1_NODE = 20
 BATCH_SIZE = 100
 
 LEARNING_RATE_BASE = 0.8
@@ -58,7 +58,65 @@ def inference(input_tensor, avg_class, weights1, biases1, weights2, biases2):
 
 
 def train(data_set):
-    return True
+    x = tf.placeholder(tf.int32, [None, INPUT_NODE], name='x-input')
+    y_ = tf.placeholder(tf.int8, [None, OUTPUT_NODE], name='y-input')
+
+    weights1 = tf.Variable(tf.truncated_normal([INPUT_NODE, LAYER1_NODE], stddev=0.1))
+    biases1 = tf.Variable(tf.constant(0.1, shape=[LAYER1_NODE]))
+
+    weights2 = tf.Variable(tf.truncated_normal([LAYER1_NODE, OUTPUT_NODE], stddev=0.1))
+    biases2 = tf.Variable(tf.constant(0.1, shape=[OUTPUT_NODE]))
+
+    y = inference(x, None, weights1, biases1, weights2, biases2)
+
+    global_step = tf.Variable(0, trainable=False)
+
+    variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+
+    variable_averages_op = variable_averages.apply(tf.trainable_variables())
+
+    average_y = inference(x, variable_averages, weights1, biases1, weights2, biases2)
+
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1))
+    cross_entropy_mean = tf.reduce_mean(cross_entropy)
+
+    regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
+    regularization = regularizer(weights1) + regularizer(weights2)
+    loss = cross_entropy_mean + regularization
+    learning_rate = tf.train.exponential_decay(
+        LEARNING_RATE_BASE,
+        global_step,
+        data_set.num / BATCH_SIZE,
+        LEARNING_RATE_DECAY)
+
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+
+    with tf.control_dependencies([train_step, variable_averages_op]):
+        train_op = tf.no_op(name='train')
+
+    correct_prediction = tf.equal(tf.argmax(average_y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+
+        validate_feed = {x: data_set.features, y_: data_set.labels}
+
+        for i in range(TRAINING_STEPS):
+            if i % 1000 == 0:
+                validate_acc = sess.run(accuracy, feed_dict=validate_feed)
+                print("After %d training step(s), validation accuracy "
+                      "using average model is %g " % (i, validate_acc))
+
+            xs, ys = data_set.next_batch(BATCH_SIZE)
+            sess.run(train_op, feed_dict={x: xs, y_: ys})
+
+
+def main():
+
+
+
+
 
 
 if __name__ == "__main__":
