c088bea0a009825a5deec05f658d090522fe5b33
OliverPan <397828546@qq.com>
2018-09-19 14:35:28 +0800

    final-version

--- train_model.py
+++ train_model.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from dataset import Data, Dataset
 import random
 import numpy as np
+import sys
 
 
 def divide_dataset(feature_filename, label_filename, ratio):
@@ -41,24 +42,24 @@ def data_import(feature_data, label_data):
         label_list = fi.readlines()
     data_set = []
     for i in range(feature_list.__len__()):
-        data = Data(list(feature_list[i])[0:24], list(label_list[i])[0:1])
+        data = Data(list(feature_list[i]), list(label_list[i])[0:1])  # list(label_list[i])[0:1] 代表的是输出数据第0位构成的列表
         data_set.append(data)
     return Dataset(data_set)
 
 
 # 数据集相关常数
-INPUT_NODE = 24
-OUTPUT_NODE = 1
+INPUT_NODE = 32     # 输入数据的位数
+OUTPUT_NODE = 1     # 输出数据的位数
 
 # 神经网络的参数
 LAYER1_NODE = 320
 
-BATCH_SIZE = 50
+BATCH_SIZE = 100
 
 LEARNING_RATE_BASE = 0.8
 LEARNING_RATE_DECAY = 0.99
 REGULARIZATION_RATE = 0.0001
-TRAINING_STEPS = 200000
+TRAINING_STEPS = 500000
 MOVING_AVERAGE_DECAY = 0.99
 
 
@@ -78,7 +79,7 @@ def inference2(input_tensor, avg_class, weights1, biases1):
         return tf.nn.relu(tf.matmul(input_tensor, avg_class.average(weights1)) + avg_class.average(biases1))
 
 
-def train(data_set, test_set, test_data):
+def train(data_set, test_set):
     x = tf.placeholder(tf.float32, [None, INPUT_NODE], name='x-input')
     y_ = tf.placeholder(tf.float32, [None, OUTPUT_NODE], name='y-input')
 
@@ -99,7 +100,7 @@ def train(data_set, test_set, test_data):
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
         validate_feed = {x: data_set.features, y_: data_set.labels}
-        test_feed = {x: test_set.features, y_: test_set.labels}
+        # test_feed = {x: test_set.features, y_: test_set.labels}
         tmp = 0
         for i in range(TRAINING_STEPS):
             if i % 1000 == 0:
@@ -110,25 +111,30 @@ def train(data_set, test_set, test_data):
                 tmp += 1
             xs, ys = data_set.next_batch(BATCH_SIZE)
             sess.run(train_step, feed_dict={x: xs, y_: ys})
-
+            if (1 - validate_acc) > 0.98:
+                print("jump out！")
+                break
+        """
         test_acc = sess.run(loss, feed_dict=test_feed)
         print("After %d training step(s), test accuracy "
               "using average model is %g " % (TRAINING_STEPS, 1 - test_acc))
         # print(sess.run(average_y, feed_dict={x: test_data, y_: np.array([[1]], dtype=np.float32)}))
+        """
 
 
 def main(argv=None):
-    divide_dataset("./data/TestData/TestData/Ra.txt", "./data/TestData/TestData/leak0.txt", 0.6)
+    divide_dataset("./data/TestData/TestData/Ra.txt", "./data/TestData/TestData/leak0.txt", 1)
+    # 读文件，第一个是输入，第二个是输出，第三个是训练集数据占数据总量的比
+    # 如果训练精度始终达不到（对于奇偶校验和海明校验）,大概率是训练集太小,需要增加训练集数据量
+
     data_set = data_import("./data/train/train_feature.data", "./data/train/train_label.data")
     test_set = data_import("./data/test/test_feature.data", "./data/test/test_label.data")
 
-    string1 = "11110010000001100000000010010100"
-    string2 = "10000100100001101010010011111011"
-    test_data = np.array([list(string2)], dtype=np.float32)
-    train(data_set, test_set, test_data)
+    train(data_set, test_set)
 
 
 if __name__ == "__main__":
+
     tf.app.run()
 
 
